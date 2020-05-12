88f128e6b9440de31298eee0731597f8e3c0bbb9
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-05-12 23:45:40 +0900

    읽고 말하기 수열(개미 수열)

--- antsequence20th/Program.cs
+++ antsequence20th/Program.cs
@@ -10,6 +10,39 @@ namespace antsequence20th
     {
         static void Main(string[] args)
         {
+            String s = "1", ss;
+            char ch;
+            int count = 0;
+            for (int i = 0; i < 19; i++)
+            {
+                Console.WriteLine(i+1 + "번째 숫자: " + s);
+                count = 0;
+                ch = s[0];
+                ss = "";
+                Console.WriteLine(s);
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (ch == s[j])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        ss = ss + ch + count;
+                        count = 0;
+                        ch = s[j];
+                        count++;
+                    }
+                    if (j + 1 == s.Length)
+                    {
+                        ss = ss + ch + count;
+                    }
+
+                }
+                s = ss;
+            }
+
+            Console.WriteLine("\n20번째 숫자: " + s);
         }
     }
 }
